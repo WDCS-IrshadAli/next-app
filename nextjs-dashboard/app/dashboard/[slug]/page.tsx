deffcbe5ea75b72f35076da41453781f1e697e51
irshadali1743 <irshadali.kadiwala@codezeros.com>
2024-02-14 11:26:22 +0530

    third commit

--- nextjs-dashboard/app/dashboard/[slug]/page.tsx
+++ nextjs-dashboard/app/dashboard/[slug]/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const page = ({ params }: {params: { slug?: string[] }}) => {
+const page = ({ params }: {params: { slug?: string }}) => {
     console.log(params);
     
   return (
     <div>
-      fgvtrhbnjyn {params?.slug?.length}
+      fgvtrhbnjyn {params?.slug}
     </div>
   )
 }
